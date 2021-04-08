99782fe467bb564d46bf89bf8b6f3a49c40f45ad
velpreso <velpreso@gmail.com>
2021-04-08 21:30:27 +0200

    ćwiczenie 3

---
+++ femlab2_cw3.py
@@ -0,0 +1,16 @@
+import numpy as np
+tab1 =np.linspace(1,5,5) #1 2 3 4 5
+tab2 =np.linspace(5,1,5) #5 4 3 2 1
+tab3 =np.linspace(0,0,2) #0 0
+tab4 =np.linspace(2,2,3) #2 2 2
+tab5 =np.linspace(-90,-70,3) #-90 -80 -70
+tab6 =np.linspace(10,10,5) # nie da się transponować na 10;10;10;10;10
+
+print(tab1)
+print(tab2)
+print(tab3)
+print(tab4)
+print(tab5)
+print(tab6)
+
+#zamysł jest taki, żeby połączyć teraz te tablice w jedną większą, niestetty nie umiem tego zrobić
